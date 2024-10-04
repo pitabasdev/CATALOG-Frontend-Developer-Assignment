f4afe3984f2d69dfcf2bffc6bf46bf762f9f8869
Pitabas Pradhan <pitabas.pradhan834@gmail.com>
2024-10-04 22:21:03 +0530

    fine update

--- src/components/BitCoinChart.js
+++ src/components/BitCoinChart.js
@@ -66,7 +66,6 @@ export default function BitCoinChart() {
           averagePrice,
         });
 
-        // Analysis Metrics
         const priceVolatility = (
           (highestPrice - lowestPrice) / lowestPrice
         ).toFixed(2);
